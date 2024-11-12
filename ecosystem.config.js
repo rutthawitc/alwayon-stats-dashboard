52e6008127d905381495ad05bf891d4470ea1670
rutthawit ch <rutthawit@gmail.com>
2024-11-12 14:31:57 +0700

    --ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,14 @@ module.exports = {
     {
       name: "alwayon-dashboard",
       script: "node_modules/next/dist/bin/next",
-      args: "start --port 3005", // กำหนด port ที่ต้องการ
-      instances: "1",
+      args: "start --port 3005",
+      instances: 1,
       exec_mode: "cluster",
       watch: false,
       max_memory_restart: "1G",
       env: {
         NODE_ENV: "production",
+        PORT: 3005,
       },
       log_date_format: "YYYY-MM-DD HH:mm:ss",
       error_file: "logs/error.log",
